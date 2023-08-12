fdf33ac537b39c575ea799f7fed291d93e153981
Abinash sahoo <avinashsahoo2021@gmail.com>
2023-08-12 13:00:23 +0530

    push the files

---
+++ src/Converter.js
@@ -0,0 +1,97 @@
+import { Button, Card, Form, Input, Select} from "antd";
+import React, { useEffect, useState } from 'react'
+import { BsCoin } from 'react-icons/bs';
+
+function Converter() {
+
+  const apiurl = 'https://api.coingecko.com/api/v3/exchange_rates';
+
+  const defaultFirstSelectValue = "Bitcoin";
+  const deafultSecondSelectValue = "Ether";
+
+  const [CryptoList, setCryptoList] = useState([]);
+  const [inputValue, setInputValue] = useState("0");
+  const [firstSelect, setFirstSelect] = useState(defaultFirstSelectValue);
+  const [secondSelect, setSecondSelect] = useState(deafultSecondSelectValue);
+  const [result, setResult] = useState("0");
+
+  useEffect(() => {
+      fetchData();
+  }, []);
+
+  async function fetchData() {
+    const response = await fetch(apiurl);
+    const jsonData = await response.json();
+
+    const data = jsonData.rates;
+    // Using For Each Loop to Convert Object into Arraylist
+    // const tempArray = [];
+    // Object.entries(data).forEach(item => {
+    //   const tempObj = {
+    //     value: item[1].name,
+    //     label: item[1].name,
+    //     rate: item[1].value
+    //   }
+    //   tempArray.push(tempObj);
+    // })
+
+    const tempArray = Object.entries(data).map(item => {
+      return {
+        value: item[1].name,
+        label: item[1].name,
+        rate: item[1].value
+      }
+    })
+    setCryptoList(tempArray);
+
+  }
+
+  useEffect( () => {
+    
+    if(CryptoList.length == 0 ) return;
+
+    const firstSelectRate = CryptoList.find((item) => {
+      return item.value === firstSelect;
+    }).rate
+
+    const secondSelectRate = CryptoList.find((item) => {
+      return item.value === secondSelect;
+    }).rate
+
+    const resultValue = (inputValue * secondSelectRate) / firstSelectRate;
+
+    setResult(resultValue.toFixed(6));
+
+  }, [inputValue, firstSelect, secondSelect]);
+
+  return (
+    <div className="container">
+       
+      <Card className="crypto-card" title ={<h1> <BsCoin/> Crypto Converter</h1>}>
+        <Form size="large">
+          <Form.Item>
+            <Input onChange={(event) => setInputValue(event.target.value)}/>
+          </Form.Item>
+        </Form>
+        <div className="select-box">
+          <Select 
+            style={{width: "200px"}} 
+            defaultValue={defaultFirstSelectValue} 
+            options={CryptoList}
+            onChange={(value) => setFirstSelect(value)}
+          />
+          <Select 
+            style={{width: "200px"}} 
+            defaultValue={deafultSecondSelectValue} 
+            options={CryptoList}
+            onChange={(value) => setSecondSelect(value)}
+          />
+        </div>
+        <p>{inputValue} {firstSelect} = {result} {secondSelect}</p>
+      </Card>
+
+    </div>
+  )
+}
+
+export default Converter
